e782a5c0d2bf6164bab035f654f6ec920353e21f
Mick Gardiner <gardiner.mick@gmail.com>
2019-11-17 15:17:44 +1030

    End of Section 6

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./app/layouts/styles.css";
-import App from "./app/layouts/App";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
